3af8c6cb22c359ae2d9646db79d0912d38f2c84e
Mushtari Sadia <midnight.scream98@gmail.com>
2020-04-02 00:10:53 +0600

    Created Home Page,About Page,Register Page
    Not finished

--- A_Helping_Hand/settings.py
+++ A_Helping_Hand/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'home_consumer.apps.HomeConsumerConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK ='bootstrap4'
---
+++ home_consumer/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class HomeConsumerConfig(AppConfig):
+    name = 'home_consumer'
