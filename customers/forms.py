e9a811e00a1903b24f9bc94a3aa1b386aac83c9d
Mushtari Sadia <midnight.scream98@gmail.com>
2020-04-06 23:20:10 +0600

    Working login and registration

--- customers/forms.py
+++ customers/forms.py
@@ -65,6 +65,8 @@ for i in AREA_LIST_ALL:
     x += 1
 
 
+
+
 class CustomerRegisterForm(UserCreationForm):
     phone_number = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}),
                                  max_length=11)
@@ -90,3 +92,59 @@ class CustomerRegisterForm(UserCreationForm):
     class Meta:
          model = User
          fields = ['phone_number','first_name','last_name','email', 'password1', 'password2','birth_year','area_field','address']
+
+
+JOB_LIST = [
+    ('1','Electrician'),
+    ('2','Home Cleaner'),
+    ('3','Pest Control Service'),
+    ('4','Plumber'),
+    ('5','Nurse'),
+    ('6','House Shifting Assistant'),
+    ('7','Carpenter')
+
+]
+
+
+
+class WorkerRegisterForm(CustomerRegisterForm):
+    phone_number = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}),
+        max_length=11)
+    email = forms.EmailField()
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}),
+                                 max_length=32)
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
+                                max_length=32)
+    birth_year = forms.DateField(widget=forms.SelectDateWidget(years=BIRTH_YEAR_CHOICES))
+    birth_year.label = "Date of birth"
+    first_name.label = "Full Name"
+    last_name.label = ""
+    area_field = forms.ChoiceField(
+        widget=forms.Select,
+        choices=AREA_LIST,
+    )
+    area_field.label = "Address"
+    address = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'House No.,Road No.,Sector/Block etc...'}),
+                                 max_length=45)
+    address.label = ""
+    job_field = forms.ChoiceField(
+        widget=forms.Select,
+        choices=JOB_LIST,
+    )
+    job_field.label = "What kind of work do you do?"
+
+
+
+    class Meta:
+        model = User
+        fields = ['phone_number','first_name','last_name','email', 'password1', 'password2','birth_year','area_field','address','job_field']
+
+
+class LoginForm(forms.Form):
+    phone_number = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Phone Number'}),
+        max_length=11)
+    phone_number.label = "Phone Number"
+    password = forms.CharField(max_length=32, widget=forms.PasswordInput)
+    password.label = "Password"
--- home/views.py
+++ home/views.py
@@ -1,11 +1,42 @@
 from django.shortcuts import render,redirect
-
+from customers.forms import LoginForm
+from django.contrib import messages
+from django.db import connection as conn
 # Create your views here.
-
+customer_id = -1
 
 def register(request):
     return render(request, 'home/register.html')
 
+def logout(request):
+    request.session['loggedIn'] = False
+    request.session['user_id'] = -1
+    messages.success(request,"You have successfully logged out")
+    return redirect('register')
 
 def login(request):
-    return render(request,'home/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            phone_number = form.cleaned_data['phone_number']
+            password = form.cleaned_data['password']
+
+            count = 0
+            for row in conn.cursor().execute("SELECT CUSTOMER_ID FROM CUSTOMER WHERE PHONE_NUMBER='" + phone_number + "' AND PASSWORD='" + password + "'"):
+                count += 1
+                customer_id = row[0]
+                #print(customer_id)
+            if count == 0 :
+                messages.warning(request, "Invalid phone number or password")
+                form = LoginForm()
+                return render(request, 'home/login.html', {'form': form})
+            else :
+                messages.success(request, "Login Successful!")
+                request.session['loggedIn'] = True
+                request.session['user_id'] = customer_id
+                return render(request,'home_customer/home.html',{'loggedIn' : request.session['loggedIn']})
+
+    else:
+        form = LoginForm()
+    return render(request, 'home/login.html', {'form': form})
+
