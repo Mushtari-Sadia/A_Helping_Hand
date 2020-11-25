1fc827bc16ae6f2e53af646ef512a1a77b0843c8
Mushtari Sadia <midnight.scream98@gmail.com>
2020-11-26 04:34:18 +0600

    Created order history table from customer end

--- home_customer/urls.py
+++ home_customer/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('home/', views.home,name='home_customer-home'),
     path('about/', views.about,name='home_customer-about'),
+    path('orders/', views.orders,name='home_customer-orders'),
 ]
--- home_customer/views.py
+++ home_customer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.db import connection
+import django_tables2 as tables
 # Create your views here.
 
 cursor = connection.cursor()
@@ -26,3 +27,62 @@ def about(request):
     else :
         return render(request, 'home_customer/about.html', {'title': 'About'})
 
+
+class OrderTable(tables.Table):
+    Order_id = tables.Column(verbose_name='Order ID')
+    Type = tables.Column(verbose_name='Service Provider')
+    Start_time = tables.Column(verbose_name='Start Time')
+    End_time = tables.Column(verbose_name='End Time')
+
+    class Meta:
+        template_name = "django_tables2/bootstrap.html"
+
+
+class PendingTable(tables.Table):
+    Type = tables.Column(verbose_name='Service Provider')
+    Description = tables.Column(verbose_name='Description')
+
+    class Meta:
+        template_name = "django_tables2/bootstrap.html"
+
+
+
+def orders(request):
+    if 'loggedIn' in request.session:
+        if 'user_type' in request.session and request.session['user_type'] == "worker":
+            return redirect('home_worker-home')
+
+        data = []
+        pending_data = []
+        if 'user_id' in request.session and request.session['user_id']!=-1:
+            customer_id = request.session['user_id']
+
+            for row in cursor.execute(
+                    "SELECT CUSTOMER_ID, ORDER_ID,TYPE,DESCRIPTION " +
+                    "FROM SERVICE_REQUEST " +
+                    "WHERE ORDER_ID = NULL " +
+                    "AND CUSTOMER_ID =" + str(customer_id) + " ;"):
+                data_dict = {}
+                data_dict['Type'] = row[2]
+                data_dict['Description'] = row[3]
+                pending_data.append(data_dict)
+
+            for row in cursor.execute(
+                    "SELECT S.CUSTOMER_ID, S.ORDER_ID, O.ORDER_ID,O.TYPE,O.START_TIME,O.END_TIME " +
+                    "FROM SERVICE_REQUEST S, ORDER_INFO O " +
+                    "WHERE S.ORDER_ID = O.ORDER_ID " +
+                    "AND S.CUSTOMER_ID =" + str(customer_id) + " ORDER BY O.START_TIME;")  :
+                data_dict = {}
+                data_dict['Order_id'] = row[2]
+                data_dict['Type'] = row[3]
+                data_dict['Start_time'] = row[4].strftime("%m/%d/%Y, %H:%M:%S")
+                data_dict['End_time'] = row[5].strftime("%m/%d/%Y, %H:%M:%S")
+                data.append(data_dict)
+
+        print(data)
+
+        ordertable = OrderTable(data)
+        pendingtable = PendingTable(pending_data)
+        return render(request, 'home_customer/orders.html',{'title' : 'Orders','loggedIn' : request.session['loggedIn'],'ordertable' : ordertable,'pendingtable' : pendingtable})
+    else :
+        return render(request, 'home_customer/homw.html', {'title': 'Home'})
